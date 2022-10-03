f8ec28d347b5d60dece071b3b73e733e1155e158
AnastasiiaBeltsova <102054895+AnastasiiaBeltsova@users.noreply.github.com>
2022-10-03 22:00:42 +0300

    Update index2.js

--- index2.js
+++ index2.js
@@ -9,31 +9,3 @@ for (let i = 0; i < 10; i++) {
 }
 document.write('</table>')
 
-
-
-// const COUNT = 10;
-//         let array = new Array(COUNT);
- 
-//         fillMatrix();
-//         console.log(array);
-//         showMatrix();
- 
-//         function fillMatrix() {
-//             for (let i = 0; i < COUNT; ++i) {
-//                 array[i] = new Array(COUNT);
-//                 for (let j = 0; j < COUNT; ++j) {
-//                     array[i][j] = i * 10 + j + 1;
-//                 }
-//             }
-//         }
-//         function showMatrix() {
-//             for (let i = 0; i < COUNT; ++i) {
-//                 for (let j = 0; j < COUNT; ++j) {
-//                     // Вывести с выравниванием
-//                     document.write(array[i][j].toString().padStart(4,"\u00A0"));
-//                     // Если выравнивание не интересует, то:
-//                     // document.write(array[i][j] +" ");
-//                 }
-//                 document.write("<br />");
-//             }
-//         }
